e7e098e2fb44bf802e8509f6c67d9fe0b76a3e49
Andrew Christie <awchristie86@gmail.com>
2021-03-21 15:39:04 -0400

    line finished

--- line.js
+++ line.js
@@ -7,7 +7,7 @@
 
 const margin = { left: 50, right: 30, top: 30, bottom: 30 };
 const height = 400 - margin.top - margin.bottom;
-const width = 600 - margin.left - margin.right;
+const width = 700 - margin.left - margin.right;
 
 const g = d3
   .select("#chart-area")
@@ -20,23 +20,41 @@ const g = d3
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear().range([height, 0]);
 
-const parseTime = d3.timeParse("%Y");
+const parseTime = d3.timeParse("%d/%m/%Y");
 const bisectDate = d3.bisector((d) => d.year).left;
 
 const xAxis = (label) => (g) => g.call(d3.axisBottom(x));
-const yAxis = (label) => (g) =>
-  g.call(
-    d3
-      .axisLeft(y)
-      .ticks(6)
-      .tickFormat((d) => `${parseInt(d / 1000)}k`)
-  );
+const yAxis = (label) => (g) => g.call(d3.axisLeft(y));
 
 const xAxisGroup = g.append("g").attr("transform", `translate(0, ${height})`);
 const yAxisGroup = g.append("g");
 
-const line = d3
-  .line()
-  .x((d) => x(d.year))
-  .y((d) => y(d.value));
+d3.json("data/coins.json").then((data) => {
+  data = data["bitcoin"];
+
+  data.forEach((row) => {
+    (row.date = parseTime(row.date)),
+      (row["24h_vol"] = +row["24h_vol"]),
+      (row.market_cap = +row.market_cap),
+      (row.price_usd = +row.price_usd);
+  });
+  x.domain(d3.extent(data, (d) => d.date));
+  y.domain([
+    d3.min(data, (d) => d.price_usd) * 0.95,
+    d3.max(data, (d) => d.price_usd) * 1.05,
+  ]);
+  const line = d3
+    .line()
+    .x((d) => x(d.date))
+    .y((d) => y(d.price_usd));
+
+  g.append("path")
+    .attr("stroke", "grey")
+    .attr("fill", "none")
+    .attr("stroke-width", "3px")
+    .attr("d", line(data));
+
+  xAxisGroup.call(xAxis("Date"));
+  yAxisGroup.call(yAxis("Price"));
+});
 
